ad2e5080389a3b2422e3b38ba1ed446c6b1f50b2
Leontjuh <82644978+Leontjuh@users.noreply.github.com>
2022-01-14 23:17:53 +0100

    test
    test

--- webdev3/scripts.js
+++ webdev3/scripts.js
@@ -7,7 +7,8 @@ databaseURL: "https://smartconnection-9d342.firebaseio.com",
 projectId: "smartconnection-9d342",
 storageBucket: "smartconnection-9d342.appspot.com",
 messagingSenderId: "467477051637",
-appId: "1:467477051637:web:79703492fcd8c86889adbc"
+appId: "1:467477051637",
+web: "79703492fcd8c86889adbc"
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
